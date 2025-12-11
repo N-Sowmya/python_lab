f9d39760fee23cbc29b6ea0310da10d8e1451be4
N-Sowmya <sowmyanallabariki2005@gmail.com>
2025-12-11 16:13:54 +0530

    Add files via upload

---
+++ expression.py
@@ -0,0 +1,8 @@
+exp1=5+3*2
+print("expression-1",exp1)
+exp2=2*3**2
+print("expression-2" ,exp2)
+exp3=2**3**2
+print("expression-3",exp3)
+exp4=(2**3)**2
+print("expression-4",exp4)
---
+++ math.py
@@ -0,0 +1,8 @@
+import math
+print("pi value",math.pi)
+a=20
+print("square root of a is",math.sqrt(a))
+angle=math.pi/3
+print("value of cos(60°) is",math.cos(angle))
+angle=math.pi/2
+print("value of sin(90°) is ",math.sin(angle))
