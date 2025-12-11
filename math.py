22020be99fbc00f4f9adbd9d7b4f3fb69e265eeb
N-Sowmya <sowmyanallabariki2005@gmail.com>
2025-12-11 19:53:28 +0530

    Delete math.py

--- math.py
+++
@@ -1,8 +0,0 @@
-import math
-print("pi value",math.pi)
-a=20
-print("square root of a is",math.sqrt(a))
-angle=math.pi/3
-print("value of cos(60°) is",math.cos(angle))
-angle=math.pi/2
-print("value of sin(90°) is ",math.sin(angle))
