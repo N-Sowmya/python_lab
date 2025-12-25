2fc6f689982dcde4e42c13ba2c9bb946d71bd4cf
N-Sowmya <sowmyanallabariki2005@gmail.com>
2025-12-25 14:54:41 +0530

    Delete class_avg.py

--- class_avg.py
+++
@@ -1,9 +0,0 @@
-marks=[6,8,3,5,9,0,6,5]
-import numpy as np
-npMarks=np.array(marks)
-print("Above average in class :",np.max(npMarks))
-print("Below average in class :",np.min(npMarks))
-print("Average in class :",np.mean(npMarks))
-print(npMarks)
-print(np.where(npMarks>5,"Above average",
-      np.where(npMarks<5,"Below Average","Average")))
